14d180031d57ec2e65a5d8f12c9ad5ad814b3280
Luke Michals <luke.michals@gmail.com>
2020-07-30 19:44:56 -0700

    add styled components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 import * as React from "react";
 import { Helmet } from "react-helmet";
 import "../styles/index.css";
+import styled from "styled-components";
+
+const RedHeader = styled.h2`
+  color: red;
+`;
 
 function Index() {
   return (
@@ -9,8 +14,9 @@ function Index() {
         <title>LGM</title>
       </Helmet>
       <h1>Portfolio goes here</h1>
+      <RedHeader>Name goes here</RedHeader>
     </main>
   );
-}
+};
 
 export default Index;
