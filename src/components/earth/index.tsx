db7886c2dddb9ed24f91844df4ad591737ded36e
Luke Michals <luke.michals@gmail.com>
2020-08-07 16:01:42 -0600

    add more versions of earth

--- src/components/earth/index.tsx
+++ src/components/earth/index.tsx
@@ -1,8 +1,7 @@
-import React, { useRef, useState } from "react";
-import { useFrame, Canvas } from "react-three-fiber";
+import React, { useRef } from "react";
 import * as THREE from "three";
 import { ReactThreeFiber } from "react-three-fiber";
-import earth from "./earth.jpg";
+import earth from "./earth-night-1k.jpg";
 
 const loader = new THREE.TextureLoader();
 
@@ -15,21 +14,8 @@ function Earth(props: EarthProps) {
     ReactThreeFiber.Object3DNode<THREE.Mesh, typeof THREE.Mesh>
   >();
 
-  const [hovered, setHover] = useState(false);
-
-  useFrame(() => {
-    if (!mesh?.current?.rotation) return;
-    mesh.current.rotation.y -= 0.0005;
-  });
-
   return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={hovered ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-      onPointerOver={(e) => setHover(true)}
-      onPointerOut={(e) => setHover(false)}
-    >
+    <mesh {...props} ref={mesh} scale={[1, 1, 1]}>
       <sphereBufferGeometry attach="geometry" args={[1, 50, 50]} />
       <meshStandardMaterial attach="material" map={loader.load(earth)} />
     </mesh>
