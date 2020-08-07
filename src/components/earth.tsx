f9e1f08c87be8b538a516967846e585db0d7ec59
Luke Michals <luke.michals@gmail.com>
2020-08-07 15:33:11 -0600

    remove 1 earth

--- src/components/earth.tsx
+++ src/components/earth.tsx
@@ -51,8 +51,7 @@ const Three = () => {
     >
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      <Box position={[0, 0, 0]} />
     </Canvas>
   );
 };
