66661caac345288f68fd3cd6b402c950bfa84391
Luke Michals <luke.michals@gmail.com>
2020-10-12 13:05:00 -0700

    add 3 remaining trapezoids

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -11,18 +11,43 @@ const StyledSVG = styled.svg`
   height: 100%;
 `;
 
+const trapezoidHeight = 20;
+
 const Tile = () => {
   return (
     <StyledTile>
       <StyledSVG viewBox="0 0 100 100" preserveAspectRatio="none">
         <path
-          d="
-            M0,0
+          d={`M0,0
             L100,0
-            l-20,20
-            L20,20
-            Z"
+            l-${trapezoidHeight},${trapezoidHeight}
+            L${trapezoidHeight},${trapezoidHeight}
+            Z`}
           fill="blue"
+        />
+        <path
+          d={`M100,0
+            L100,100
+            l-${trapezoidHeight},-${trapezoidHeight}
+            L${100 - trapezoidHeight},${trapezoidHeight}
+            Z`}
+          fill="red"
+        />
+        <path
+          d={`M100,100
+            L0,100
+            l${trapezoidHeight},-${trapezoidHeight}
+            L${100 - trapezoidHeight},${100 - trapezoidHeight}
+            Z`}
+          fill="green"
+        />
+        <path
+          d={`M0,100
+            L0,0
+            l${trapezoidHeight},${trapezoidHeight}
+            L${ trapezoidHeight},${100 - trapezoidHeight}
+            Z`}
+          fill="yellow"
         />
       </StyledSVG>
     </StyledTile>
