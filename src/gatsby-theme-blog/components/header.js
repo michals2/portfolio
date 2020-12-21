7b7d2f0fddc9d25d1dee47ef8fb03cc7604561a9
Luke Michals <luke.michals@gmail.com>
2020-12-21 15:59:40 -0700

    add back in dark mode switch

--- src/gatsby-theme-blog/components/header.js
+++ src/gatsby-theme-blog/components/header.js
@@ -1,7 +1,10 @@
 /** @jsx jsx */
 import { Link } from "gatsby"
-import { jsx, css, Styled } from "theme-ui"
-import Bio from "./bio"
+import { jsx, css, useColorMode, Styled } from "theme-ui"
+import Switch from "./switch"
+import Bio from "gatsby-theme-blog/src/components/bio"
+import sun from "../../assets/sun.png"
+import moon from "../../assets/moon.png"
 
 const rootPath = `${__PATH_PREFIX__}/`
 
@@ -51,30 +54,69 @@ const Title = ({ children, location }) => {
   }
 }
 
-const Header = ({ children, title, ...props }) => (
-  <header>
-    <div
-      css={css({
-        maxWidth: `container`,
-        mx: `auto`,
-        px: 3,
-        pt: 4,
-      })}
-    >
+const iconCss = [{ pointerEvents: `none`, margin: 4 }]
+
+const checkedIcon = (
+  <img
+    alt="moon indicating dark mode"
+    src={moon}
+    width="16"
+    height="16"
+    role="presentation"
+    css={iconCss}
+  />
+)
+
+const uncheckedIcon = (
+  <img
+    alt="sun indicating light mode"
+    src={sun}
+    width="16"
+    height="16"
+    role="presentation"
+    css={iconCss}
+  />
+)
+
+const Header = ({ children, title, ...props }) => {
+  const [colorMode, setColorMode] = useColorMode()
+  const isDark = colorMode === `dark`
+  const toggleColorMode = (e) => {
+    setColorMode(isDark ? `light` : `dark`)
+  }
+
+  return (
+    <header>
       <div
         css={css({
-          display: `flex`,
-          justifyContent: `space-between`,
-          alignItems: `center`,
-          mb: 4,
+          maxWidth: `container`,
+          mx: `auto`,
+          px: 3,
+          pt: 4,
         })}
       >
-        <Title {...props}>{title}</Title>
-        {children}
+        <div
+          css={css({
+            display: `flex`,
+            justifyContent: `space-between`,
+            alignItems: `center`,
+            mb: 4,
+          })}
+        >
+          <Title {...props}>{title}</Title>
+          <Switch
+            aria-label={`Toggle dark mode ${isDark ? `off` : `on`}`}
+            checkedIcon={checkedIcon}
+            uncheckedIcon={uncheckedIcon}
+            checked={isDark}
+            onChange={toggleColorMode}
+          />
+          {children}
+        </div>
+        {props.location.pathname === rootPath && <Bio />}
       </div>
-      {props.location.pathname === rootPath && <Bio />}
-    </div>
-  </header>
-)
+    </header>
+  )
+}
 
 export default Header
