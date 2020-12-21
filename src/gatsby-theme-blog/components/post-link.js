1e5d2fe1d6891ccc2a6f9a0057d97e60b98426ef
Luke Michals <luke.michals@gmail.com>
2020-12-21 14:37:18 -0700

    add tags to posts

---
+++ src/gatsby-theme-blog/components/post-link.js
@@ -0,0 +1,33 @@
+/** @jsx jsx */
+import { Styled, jsx } from "theme-ui"
+import { Link } from "gatsby"
+
+const PostLink = ({ title, slug, date, tags }) => (
+  <article>
+    <header>
+      <Styled.h2
+        sx={{
+          mb: 1,
+        }}
+      >
+        <Styled.a
+          as={Link}
+          sx={{
+            textDecoration: `none`,
+          }}
+          to={slug}
+        >
+          {title || slug}
+        </Styled.a>
+      </Styled.h2>
+      <small>{date}</small>
+    </header>
+    <section>
+      {tags.map((tag, i) =>
+        i === 0 ? <span>{tag}</span> : <span> &bull; {tag}</span>
+      )}
+    </section>
+  </article>
+)
+
+export default PostLink
