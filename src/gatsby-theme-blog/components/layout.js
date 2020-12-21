d5a1a46e1d3cb408ee8260be0b24469d17655f22
Luke Michals <luke.michals@gmail.com>
2020-12-21 14:57:34 -0700

    remove skip to content popup

---
+++ src/gatsby-theme-blog/components/layout.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { css, Styled } from "theme-ui"
+import Header from "./header"
+import useBlogThemeConfig from "../hooks/configOptions"
+import Helmet from "react-helmet"
+
+const Layout = ({ children, ...props }) => {
+  const blogThemeConfig = useBlogThemeConfig()
+  const { webfontURL } = blogThemeConfig
+
+  return (
+    <Styled.root>
+      <Helmet>
+        <link rel="stylesheet" href={webfontURL} />
+      </Helmet>
+      <Header {...props} />
+      <div>
+        <div
+          css={css({
+            maxWidth: `container`,
+            mx: `auto`,
+            px: 3,
+            py: 4,
+          })}
+        >
+          {children}
+        </div>
+      </div>
+    </Styled.root>
+  )
+}
+
+export default Layout
