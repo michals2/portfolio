603073f1fda255374be499fe95d66f339c7f10e1
Luke Michals <luke.michals@gmail.com>
2021-05-13 13:13:33 -0700

    add bitcoin chart

--- src/posts/bitcoins-per-person/Bitcoin.js
+++ src/posts/bitcoins-per-person/Bitcoin.js
@@ -1,17 +1,15 @@
 import React from "react"
-import data from "./world-population-data.json"
+import data from "./bitcoin-supply.json"
 import { LineChart, XAxis, YAxis, CartesianGrid, Line } from "recharts"
 
 const Population = () => {
   return (
     <div>
       <LineChart width={500} height={300} data={data} margin={{ bottom: 20 }}>
-        <XAxis label={{ value: "Year", dy: 15 }} dataKey="year" interval={20} />
-        <YAxis
-          label={{ value: "Population (Billions)", angle: -90, dx: -10 }}
-        />
+        <XAxis label={{ value: "Date", dy: 15 }} dataKey="date" interval={10} />
+        <YAxis label={{ value: "Supply (Millions)", angle: -90, dx: -10 }} />
         <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-        <Line type="monotone" dataKey="population" stroke="#8884d8" />
+        <Line type="monotone" dataKey="supply" stroke="#8884d8" />
       </LineChart>
     </div>
   )
