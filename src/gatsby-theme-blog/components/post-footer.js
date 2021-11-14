ddf03dd57d89c4c2b81a17d766fb36b8f0af705e
michals2 <luke.michals@gmail.com>
2021-11-14 13:36:50 -0700

    fix margins in footer

---
+++ src/gatsby-theme-blog/components/post-footer.js
@@ -0,0 +1,46 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { css, Themed, Flex, Box } from "theme-ui"
+import Bio from "../components/bio"
+
+const PostFooter = ({ previous, next }) => (
+  <footer
+    css={css({
+      mt: 4,
+      pt: 3,
+    })}
+  >
+    <Themed.hr />
+    <Box m={3}>
+      <Bio />
+    </Box>
+    {(previous || next) && (
+      <Flex
+        as="ul"
+        css={css({
+          flexWrap: `wrap`,
+          justifyContent: `space-between`,
+          listStyle: `none`,
+          padding: 0,
+        })}
+      >
+        <li>
+          {previous && (
+            <Themed.a as={Link} to={previous.slug} rel="prev">
+              ← {previous.title}
+            </Themed.a>
+          )}
+        </li>
+        <li>
+          {next && (
+            <Themed.a as={Link} to={next.slug} rel="next">
+              {next.title} →
+            </Themed.a>
+          )}
+        </li>
+      </Flex>
+    )}
+  </footer>
+)
+
+export default PostFooter
