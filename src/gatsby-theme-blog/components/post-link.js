08a7545ad056bd157bfc01fc0ce2e4b81ccc1c24
michals2 <lgm1234@protonmail.com>
2022-05-03 08:42:25 -0700

    add ICE in browser article

--- src/gatsby-theme-blog/components/post-link.js
+++ src/gatsby-theme-blog/components/post-link.js
@@ -5,28 +5,33 @@ import { Link } from "gatsby"
 const PostLink = ({ title, slug, date, tags }) => {
   console.log({ title, slug, date, tags })
   return (
-    <article>
+    <article
+      css={css({
+        marginBottom: "50px",
+      })}
+    >
       <header>
-        <Themed.h2 sx={{ mb: 1 }}>
+        <Themed.h2
+          css={css({
+            marginBottom: "0",
+          })}
+        >
           <Themed.a as={Link} sx={{ textDecoration: `none` }} to={slug}>
             {title || slug}
           </Themed.a>
         </Themed.h2>
         <small>{date}</small>
       </header>
-      <section
-        css={css({
-          fontSize: "8px",
-        })}
-      >
-        {tags && tags.map((tag, i) =>
-          i === 0 ? (
-            <span key={i}>{tag}</span>
-          ) : (
-            <span key={i}> &bull; {tag}</span>
-          )
-        )}
-      </section>
+      <small>
+        {tags &&
+          tags.map((tag, i) =>
+            i === 0 ? (
+              <span key={i}>{tag}</span>
+            ) : (
+              <span key={i}> &bull; {tag}</span>
+            )
+          )}
+      </small>
     </article>
   )
 }
